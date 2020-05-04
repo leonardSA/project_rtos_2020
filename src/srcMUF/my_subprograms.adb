13d3df8e057c44eccd835b80ecf67f02b8c8e799
leonardSA <leonard.stephenauguste@gmail.com>
2020-05-04 21:13:59 +0200

    MUF,subprg: add fourth task

--- src/srcMUF/my_subprograms.adb
+++ src/srcMUF/my_subprograms.adb
@@ -20,6 +20,11 @@ package body my_subprograms is
       my_subprograms.message (id3);
    end T3;
 
+   procedure T4 is
+   begin
+      my_subprograms.message (id4);
+   end T4;
+
    protected body my_subprograms is
       procedure message (id : Integer) is
          a_tcb : constant tcb := user_level_scheduler.get_tcb (id);
--- src/srcMUF/my_subprograms.ads
+++ src/srcMUF/my_subprograms.ads
@@ -1,10 +1,11 @@
 package my_subprograms is
 
-   id1, id2, id3 : Integer;
+   id1, id2, id3, id4 : Integer;
 
    procedure T1;
    procedure T2;
    procedure T3;
+   procedure T4;
 
    protected my_subprograms is
       procedure message (id : Integer);
